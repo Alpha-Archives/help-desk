015d15006e959fd1bca6934deee7ff8b58a91b0c
trim996 <gsonly.imo@gmail.com>
2021-03-26 01:05:56 +0300

    remove initialState param infav of ES6 default argumets

--- src/store.js
+++ src/store.js
@@ -1,15 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-
-const initialState = {};
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
