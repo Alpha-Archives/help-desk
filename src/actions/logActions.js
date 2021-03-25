716045da0665f5fd482f7889eb836314a7d1a6ad
trim996 <gsonly.imo@gmail.com>
2021-03-26 01:27:56 +0300

    refactor: add a function to parse your json

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -7,15 +7,17 @@ import {
   UPDATE_LOG,
   SEARCH_LOGS,
   SET_CURRENT,
-  CLEAR_CURRENT
-} from './types';
+  CLEAR_CURRENT,
+} from "./types";
+
+import parseJSON from "../utils";
 
 // export const getLogs = () => {
 //   return async dispatch => {
 //     setLoading();
 
 //     const res = await fetch('/logs');
-//     const data = await res.json();
+//     const data = await parseJSON(res);
 
 //     dispatch({
 //       type: GET_LOGS,
@@ -25,137 +27,137 @@ import {
 // };
 
 // Get logs from server
-export const getLogs = () => async dispatch => {
+export const getLogs = () => async (dispatch) => {
   try {
     setLoading();
 
-    const res = await fetch('/logs');
-    const data = await res.json();
+    const res = await fetch("/logs");
+    const data = await parseJSON(res);
 
     dispatch({
       type: GET_LOGS,
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
 // Add new log
-export const addLog = log => async dispatch => {
+export const addLog = (log) => async (dispatch) => {
   try {
     setLoading();
 
-    const res = await fetch('/logs', {
-      method: 'POST',
+    const res = await fetch("/logs", {
+      method: "POST",
       body: JSON.stringify(log),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
-    const data = await res.json();
+    const data = await parseJSON(res);
 
     dispatch({
       type: ADD_LOG,
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
 // Delete log from server
-export const deleteLog = id => async dispatch => {
+export const deleteLog = (id) => async (dispatch) => {
   try {
     setLoading();
 
     await fetch(`/logs/${id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     });
 
     dispatch({
       type: DELETE_LOG,
-      payload: id
+      payload: id,
     });
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
 // Update log on server
-export const updateLog = log => async dispatch => {
+export const updateLog = (log) => async (dispatch) => {
   try {
     setLoading();
 
     const res = await fetch(`/logs/${log.id}`, {
-      method: 'PUT',
+      method: "PUT",
       body: JSON.stringify(log),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
 
-    const data = await res.json();
+    const data = await parseJSON(res);
 
     dispatch({
       type: UPDATE_LOG,
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
 // Search server logs
-export const searchLogs = text => async dispatch => {
+export const searchLogs = (text) => async (dispatch) => {
   try {
     setLoading();
 
     const res = await fetch(`/logs?q=${text}`);
-    const data = await res.json();
+    const data = await parseJSON(res);
 
     dispatch({
       type: SEARCH_LOGS,
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
 // Set current log
-export const setCurrent = log => {
+export const setCurrent = (log) => {
   return {
     type: SET_CURRENT,
-    payload: log
+    payload: log,
   };
 };
 
 // Clear current log
 export const clearCurrent = () => {
   return {
-    type: CLEAR_CURRENT
+    type: CLEAR_CURRENT,
   };
 };
 
 // Set loading to true
 export const setLoading = () => {
   return {
-    type: SET_LOADING
+    type: SET_LOADING,
   };
 };
